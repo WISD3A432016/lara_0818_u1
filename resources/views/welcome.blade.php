991093f0a7c825af2de8a1eea4937f95731c6084
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-12-20 11:05:43 +0800

    Print View

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,11 @@
                     Laravel
                 </div>
 
+                <div class="visible-print text-center">
+                    {!! QrCode::size(100)->generate(Request::url()); !!}
+                    <p>Scan me to return to the original page.</p>
+                </div>
+
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
